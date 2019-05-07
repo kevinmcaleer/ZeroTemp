9d970120156d493f69fcc69c3c9ec5799e10efc8
Kevin McAleer <kevinmcaleer@me.com>
2019-05-07 13:27:45 +0100

    moved the import of sleep, time etc to above gpiozero as per Python Linter suggestion

--- zerotemp.py
+++ zerotemp.py
@@ -1,5 +1,5 @@
+from time import sleep, strftime, time # should be placed before gpiozero
 from gpiozero import CPUTemperature
-from time import sleep, strftime, time
 import matplotlib.pyplot as plt
 
 cpu = CPUTemperature()
@@ -7,7 +7,7 @@ cpu = CPUTemperature()
 plt.ion()
 x = []
 y = []
-
+    
 def write_temp(temp):
     with open("/home/pi/cpu_temp.csv", "a") as log:
         log.write("{0},{1}".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
